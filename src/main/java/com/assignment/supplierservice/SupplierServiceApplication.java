a4cc9dfe5b6e5cd24dcde6f60b5ad578aecd9f8b
Marta Tkacz <MartaTkacz>
2022-04-24 13:39:51 +0100

    Adding tomcat dependencies

--- src/main/java/com/assignment/supplierservice/SupplierServiceApplication.java
+++ src/main/java/com/assignment/supplierservice/SupplierServiceApplication.java
@@ -2,9 +2,10 @@ package com.assignment.supplierservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SupplierServiceApplication {
+public class SupplierServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupplierServiceApplication.class, args);
