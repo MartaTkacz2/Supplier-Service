de1acb8e5e5481a3dd0fd24382be978c75a06c9a
Marta Tkacz <MartaTkacz>
2022-04-24 18:57:42 +0100

    Test commit

--- src/test/java/com/assignment/supplierservice/SupplierServiceApplicationTests.java
+++ src/test/java/com/assignment/supplierservice/SupplierServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.assignment.supplierservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+@DisplayName("PartnerMapSkuWebController Unit Tests")
+@ExtendWith(MockitoExtension.class)
 class SupplierServiceApplicationTests {
 
 	@Test
